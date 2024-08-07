6901519daf15380d0b8e08736dcb7070a5f80cda
hexifyr <hexifyr@proton.me>
2024-08-07 17:05:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,69 @@
-function openDiscordApp() {
-    var userId = "1236035473625841735"; // Your Discord User ID Here
+const container = document.querySelector("#container"),  
+    tile = document.querySelector(".tile");
 
-    // check that the user has installed discord
-    if (window.navigator && window.navigator.msLaunchUri) {
-      // for Webbrowser
-      window.navigator.msLaunchUri("discord://user?id=" + userId);
-    } else if (window.navigator && window.navigator.userAgent.match(/(android|iphone|ipad)/i)) {
-      // for andorid and ios devices
-      window.location.href = "discord://user?id=" + userId;
-    } else {
-      // for other devices or webbrowsers
-      window.open("https://discord.com/users/" + userId);
-    }
-  }
-
-  function myFunction() {
-    var copyText = "LUejdqgSQZEdhgFonr5SMfwJfThPmTLyDU";
-    navigator.clipboard.writeText(copyText);
-    
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Wallet Address Copied!";
-
-    var styles = `  
-  .tooltip .tooltiptext {
-    visibility: visible;
-    opacity: 1;
-    
-  }
-    
-  
-  
-  `
-  
-
-  
-  var styles2 = `  
-  .tooltip .tooltiptext {
-    visibility: visible;
-    opacity: 0;
-    
-  }
-    
-  
-  
-  `
-
-
-
-  var stylesheet = document.createElement("style");
-  stylesheet.textContent = styles;
-  document.head.appendChild(stylesheet);
-
-  setTimeout(() => {
-    
-  stylesheet.textContent = styles2;
-  }, 500);
-
-  }
-  
-  function outFunc() {
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Kopieer Litecoin Adres";
-  }
-
-  function updateTime() {
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    
-    const now = new Date();
-    const day = days[now.getDay()];
-    const month = months[now.getMonth()];
-    const date = now.getDate();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-
-    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-
-    const formattedTime = `${day}, ${month} ${date} ${formattedHours}:${formattedMinutes} ${ampm}`;
-    document.getElementById('time').textContent = formattedTime;
+for(let i = 0; i < 6299; i++) {
+  container.appendChild(tile.cloneNode());
 }
 
-// Update the time immediately and then every second
-updateTime();
-setInterval(updateTime, 1000);
+
+// document.querySelectorAll('.buttons a').forEach((element, index, elements) => {
+//     element.addEventListener('mouseover', (event) => {
+//         // Reset all elements first
+//         elements.forEach((el, i) => {
+//             // el.style.width = '40px';
+//             element.style.transform = 'translateY(0px)';
+//         });
+
+//         // Adjust hovered element
+//         // element.style.width = '50px';
+
+//         // Adjust the next sibling
+//         if (elements[index + 1]) {
+//             // elements[index + 1].style.width = '45px';
+//         }
+
+//         if (elements[index + 2]) {
+//             // elements[index + 2].style.width = '42.5px';
+//         }
+
+//         if (elements[index - 1]) {
+//             // elements[index - 1].style.width = '45px';
+//         }
+       
+//         if (elements[index - 2]) {
+//             // elements[index - 2].style.width = '42.5px';
+//         }
+//         element.style.transform = 'translateY(-5px)';
+
+      
+//     });
+
+//     element.addEventListener('mouseout', () => {
+//         // Reset all elements when mouse leaves
+//         elements.forEach((el) => {
+//             // el.style.width = '40px';
+//             element.style.transform = 'translateY(0px)';
+//         });
+//     });
+// });
+
+
+
+
+
+let imageToSpin = document.getElementById('avatar');
+
+function spinImage(imageToSpin) {
+ imageToSpin.style.animationName = 'spin';
+ imageToSpin.style.animationDuration = '1s';
+ setTimeout(() => {
+    
+ imageToSpin.style.animationDuration = '3s';
+ imageToSpin.style.animationName = 'floating2';
+ }, 1000);
+}
+
+
+imageToSpin.addEventListener("click", function() {
+    spinImage(imageToSpin);
+})
